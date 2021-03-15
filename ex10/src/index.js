4170021b8b7a06b1914c779d37289ed9cf7464bf
Adis012 <adis012@hotmail.com>
2021-03-16 00:50:05 +0100

    added project

--- ex10/src/index.js
+++ ex10/src/index.js
@@ -1,18 +1,22 @@
-function mathTest(x, y) {
-    console.log("x, y");
-    return ("0");
-    console.log("undefined")
-}
-
-mathTest();
-
-
+// Only change code belowe this line
+function mathTest(x, y) {}
+// Only change code above this line
 if (x < 0 || y < 0) {
-
-
-
+    // Only change code belowe this line
+    return undefined;
+    // Only change code above this line
 }
-return Math.round(Math.pow(Math.sqrt(x) + Math.sqrt(y), 2));
+return Math.round(Math.pow(Math.sqrt(x) + Math.sqrt(y), 2)); 
 
+// Only change code belowe this line
+console.log(2, 2);
+console.log(2, 2);
+console.log(-2, 2);
+console.log(2, -2);
+console.log(2, 8);
+console.log(3, 3);
+console.log(0, 0);
+mathTest(2, 2);
 
-module.exports = mathTest;
+// Only change code above this line
+module.exports = mathTest
